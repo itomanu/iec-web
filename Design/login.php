4d769dfcb599c6fe6461b10ac6293bf563ea9aa6
Albert Christanto <albertchristanto@yahoo.com>
2012-09-21 15:40:26 +0700

    commit design login

---
+++ Design/login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="mystyle.css" />
+		<title>
+			Login
+		</title>
+	</head>
+
+	<body>
+		<div id="header">
+			<a href="#"><img src="image/iec-logo.png" alt="action"/></a>
+		</div>	
+			
+		<div id="center">
+			<div id="content">
+			<div class="form-login">
+				<form>
+					<table>
+						<tr>
+							<td>Username:<td/>
+							<td><input type="text" name="username" /><td/>
+						</tr>
+						<tr>
+							<td>Password:<td/>
+							<td><input type="password" name="password" /><td/>
+						</tr>
+						<tr>
+							<td><td/>
+							<td align="right"><input type="submit" value="Submit" /><td/>
+						</tr>
+					</table>
+				</form>
+			</div>
+			</div>
+		</div>
+		
+		<div id="menu_bottom">
+			<ul>
+				<li>
+					<a href="#">
+					<img src="image/student.png" /><br/>Student
+					</a>
+				</li>
+				<img src="image/separator.png" />
+				<li>
+					<a href="#">
+					<img src="image/jurnal.png" /><br/>Journal
+					</a>
+				</li>
+				<img src="image/separator.png" />
+				<li>
+					<a href="#">
+					<img src="image/contact.jpg" /><br/>Contact Us
+					</a>
+				</li>
+				<img src="image/separator.png" />
+				<li>
+					<a href="#">
+					<img src="image/ithb.png" /><br/>ITHB
+					</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div id="footer">
+		<p><a href="">Informatics Engineering Community</a> &copy; 2012<br><span style="font-size:10px">*best resolution in 1024*768</span></p>
+		</div>
+	</body>
+</html>
